df2081112290406e67dfc6376ce06961250276ec
vitorrubi <106936032+vitorrubi@users.noreply.github.com>
2023-05-16 20:35:05 -0300

    .

---
+++ Front/src/App.jsx
@@ -0,0 +1,16 @@
+import React, { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "../public/vite.svg";
+import "./App.css";
+
+import Signup from "./users/signup";
+
+function App() {
+  return (
+    <>
+      <Signup />
+    </>
+  );
+}
+
+export default App;
