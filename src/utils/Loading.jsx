7c134fd00e1f25c5042b3851678bde479367576d
gcmarcello <marcellogcoelho@outlook.com>
2023-05-23 18:26:25 -0300

    feat: loading component

---
+++ src/utils/Loading.jsx
@@ -0,0 +1,11 @@
+const Loading = () => {
+  return (
+    <div className="w-100 v-100 d-flex justify-content-center my-2">
+      <div className="spinner-border" role="status">
+        <span className="visually-hidden">Loading...</span>
+      </div>
+    </div>
+  );
+};
+
+export default Loading;
