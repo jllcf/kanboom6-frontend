01cd78284c7d44f6a206c3f6f228e29b311a6240
gcmarcello <marcellogcoelho@outlook.com>
2023-05-23 18:31:08 -0300

    refactor: rebuilt login page with rhf

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,62 +1,131 @@
-import React, { useState } from "react";
-import logo from "../images/Wimv6FO - Imgur 1 (1).png";
+import { useState } from "react";
+import logo from "../../assets/logo.png";
+import Loading from "../../utils/Loading";
 import "./css/Login.css";
 
-import api from "../config/configApi";
+import { useForm } from "react-hook-form";
+import { Link } from "react-router-dom";
 
 function Login() {
-  const [users, setDados] = useState({
-    email: "",
-    password: "",
-  });
+  const {
+    register,
+    handleSubmit,
+    setError,
+    formState: { errors },
+  } = useForm();
 
-  const valorInput = (e) => setDados({ ...users, [e.target.name]: e.target.value });
+  const [isLoading, setIsLoading] = useState(false);
 
-  const loginSubmit = async (e) => {
-    e.preventDefault();
-    //enviar dados para API
-
-    const headers = {
-      "Content-Type": "application/json",
-    };
-
-    await api
-      .post("/login", users, { headers })
-      .then((response) => {
-        console.log(response);
-      })
-      .catch((err) => {
-        newFunction();
-
-        function newFunction() {
-          console.log("Erro: tente novamente.");
-        }
+  const onSubmit = async (data) => {
+    setIsLoading(true);
+    try {
+      const response = await fetch("/api/login/", {
+        headers: {
+          "Content-Type": "application/json",
+        },
+        method: "POST",
+        body: JSON.stringify(data),
       });
+      const parseResponse = await response.json();
+      if (parseResponse.type === "success") {
+        localStorage.setItem("token", parseResponse.token);
+      } else {
+        setError("root.serverError", {
+          type: "401",
+          message: parseResponse.message,
+        });
+      }
+    } catch (error) {
+      console.error("Erro ao enviar formulário", error);
+    } finally {
+      setIsLoading(false);
+    }
   };
 
   return (
     <div className="container">
-      <img className="img" src={logo} alt="logo" />
-      <h3>Log In</h3>
-      <div className="wrap-form">
-        <form className="form" onSubmit={loginSubmit}>
-          <div>
-            <input type="email" name="email" placeholder="e-mail" onChange={valorInput} />
+      <div className="row justify-content-center">
+        <div className="col-10 col-md-8 col-lg-4 bg-white rounded-2 my-3">
+          <div className="row">
+            <div className="col-12 d-flex justify-content-center">
+              <img className="img" src={logo} alt="logo" />
+            </div>
           </div>
-          <div>
-            <input type="text" name="password" placeholder="Password" onChange={valorInput} />
+          <div className="row">
+            <div className="col-12 d-flex justify-content-center">
+              <h3 className="login-title">Fazer Login no Kanboom</h3>
+            </div>
           </div>
-          <div>
-            <button className="button" type="submit">
-              Entrar
-            </button>
+          {errors && errors?.root?.serverError?.message && (
+            <div className="alert alert-danger mt-3">
+              <ul className="d-flex  align-items-center mb-0 p-0">
+                {[errors.root.serverError.message].map((error, index) => (
+                  <li className="list-group-item" key={`error-${index}`}>
+                    <i className="bi bi-exclamation-triangle-fill me-2"></i> {error}
+                  </li>
+                ))}
+              </ul>
+            </div>
+          )}
+          <form className="form" onSubmit={handleSubmit(onSubmit)}>
+            <div className="row">
+              <div className="col-12">
+                <input
+                  type="email"
+                  name="email"
+                  className="form-control my-2"
+                  placeholder="E-mail"
+                  {...register("user_email", {
+                    required: true,
+                  })}
+                  disabled={isLoading}
+                />
+              </div>
+            </div>
+            <div className="row">
+              <div className="col-12">
+                <input
+                  type="password"
+                  name="password"
+                  className="form-control my-2"
+                  placeholder="Senha"
+                  {...register("user_password", {
+                    required: true,
+                  })}
+                  disabled={isLoading}
+                />
+              </div>
+            </div>
+            <div className="row mt-2 mb-3">
+              <div className="col-12">
+                {isLoading ? (
+                  <Loading />
+                ) : (
+                  <button className="btn-primary-kanboom form-control" type="submit">
+                    entrar
+                  </button>
+                )}
+              </div>
+            </div>
+          </form>
+          <div className="mb-2">
+            <div className="row">
+              <div className="col-12 d-flex justify-content-center text-center">
+                <Link to="/" className="login-paragraph mb-0 text-decoration-none">
+                  Esqueci Minha Senha
+                </Link>
+              </div>
+            </div>
+
+            <div className="row">
+              <div className="col-12 d-flex justify-content-center text-center">
+                <Link to="/" className="login-paragraph mb-0 text-decoration-none">
+                  Ainda não tem uma conta? Clique <span className="text-decoration-underline">aqui</span>.
+                </Link>
+              </div>
+            </div>
           </div>
-          <div>
-            <button className="button" type="reset">
-              Limpar
-            </button>
-          </div>
-        </form>
+        </div>
       </div>
     </div>
   );
